659dbeb7c1c9948717e985d4f2fa77e92c3eefb5
raed <raed.da3k@gmail.com>
2023-05-19 12:31:21 +0100

    pall func

---
+++ pallfunc.c
@@ -0,0 +1,21 @@
+#include"monty.h"
+
+/**
+ * _pall - print elements stack
+ * @stack: is a parameter
+ * @n: is value
+ * Return: nothing
+ */
+void _pall(stack_t **stack, unsigned int n)
+{
+	stack_t *current = NULL;
+	(void)n;
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		dprintf(STDOUT_FILENO, "%d\n", current->n);
+		current = current->next;
+	}
+}
