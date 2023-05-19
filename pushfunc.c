fcd4eb3f8255de5ee1d2134e024af7ac1d7b6d7e
raed <raed.da3k@gmail.com>
2023-05-19 12:31:38 +0100

    push function

---
+++ pushfunc.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * _push - push item
+ * @stack: is a parameter
+ * @line_number: is value
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	(void)line_number;
+
+	new = new_Node(value);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
