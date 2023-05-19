87808b9122a1e3b302c7fa2b9c9e0ed18285459f
raed <raed.da3k@gmail.com>
2023-05-19 11:48:01 +0100

    header file for monty interpter

---
+++ monty.h
@@ -0,0 +1,55 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> /*defines data types used in system source code*/
+#include <errno.h> /*defines macros for reporting and retrieving error conditions*/
+#include <sys/stat.h> /*getting information about files attributes.*/
+#include <fcntl.h> /*contains constructs that refer to file control*/
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern glob_t global;
+extern int value;
+
+/* function prototypes */
+void push(stack_t **stack, int value);
+void pall(stack_t **stack);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
